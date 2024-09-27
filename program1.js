e613fb6229a1c5198b890a5eec5b96c68150fd8e
rajvardhan1008 <128119170+rajvardhan1008@users.noreply.github.com>
2024-09-27 12:05:00 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,5 +1,26 @@
 const getTotalIsles = function (grid) {
 
+  let cnt = 0;
+
+    const recursive = (row, col) => {
+      grid[row][col] = 2;
+
+      if (grid[row][col + 1] === '1') recursive(row, col + 1);
+      if (grid[row + 1]?.[col] === '1') recursive(row + 1, col);
+      if (grid[row][col - 1] === '1') recursive(row, col - 1);
+      if (grid[row - 1]?.[col] === '1') recursive(row - 1, col);
+    };
+
+    for (let row = 0; row < grid.length; row++) {
+      for (let col = 0; col < grid[row].length; col++) {
+        if (grid[row][col] === '1') {
+          cnt++;
+          recursive(row, col);
+        }
+      }
+    }
+
+    return cnt;
   
 };
 
